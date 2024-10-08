87e8b28ca8ada2aba0024d50f8c695d1f0d35295
Yajnesh Kumar <113285428+yajneshkumar790@users.noreply.github.com>
2024-10-08 20:07:00 +0530

    first commit

---
+++ app/src/main/java/com/example/ccedepartmentapplication/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.ccedepartmentapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton menuToggleButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menuToggleButton = findViewById(R.id.menuToggleButton);
+
+        menuToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+    }
+}
